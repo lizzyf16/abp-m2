c6d6a41008b3a0c2c077d7476fd7a3effb2ab35e
Liz Fernandez <liz.maryfernandez@gmail.com>
2026-06-12 12:09:06 -0400

    modificaciones en menu, cración de modales y form de sendmoney

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,3 +1,9 @@
+// $(document).ready(function() {
+//   // Inicializar saldo en localStorage si no existe
+//   if (localStorage.getItem("saldo") === null) {
+//     localStorage.setItem("saldo", "100000"); // 100_000 inicial
+//   }
+
 //Variables
 
 let balanceInicial = 100000;
@@ -15,3 +21,33 @@ console.log ("Valor del Elemento:", montoBalance.innerText)
 
  //MENSAJE AL USUARIO DE ÉXITO
 alert(`Se agregó un total de: ${montoDeposit}, siendo su nuevo balance: ${nuevoBalance}`);
+
+function mensajeDeposit (){
+    alert("Redirigiendo a Depositar.");
+    window.location.href = "./deposit.html"    
+};
+
+function mensajeSendmoney (){
+    alert("Redirigiendo a Enviar Dinero.");
+    window.location.href = "./sendmoney.html"    
+};
+function mensajeTransaction (){
+    alert("Redirigiendo a Últimos Movimientos.");
+    window.location.href = "./transactions.html"    
+};
+
+// $("#btnDeposit").on("click", function() {
+//     alert(`Redirigiendo a Depositar`);
+//     window.location.href = "./deposit.html";
+//   });
+
+
+//   $("#buttonSendMoneyt").on("click", function() {
+//     alert("Redirigiendo a Enviar Dinero...");
+//     window.location.href ="./sendmoney.html";
+//   });
+  
+//   $("#buttonTransaction").on("click", function() {
+//     alert("Redirigiendo a Transacciones...");
+//     window.location.href = "./transactions.html";
+//   });
--- assets/js/sendmoney.js
+++ assets/js/sendmoney.js
@@ -10,38 +10,38 @@ balanceEl.textContent = balance.toLocaleString("es-CL");
 //SIMULAMOS LOS REGISTROS DE CONTACTO DEL CLIENTE
 
 const contactos = [
-{
-id: 1,
-nombre: "John Doe",
-cbu: "123456789",
-alias: "john.doe",
-banco: "ABC Bank",
-},
-{
-id:2,
-nombre: "Jane Smith",
-cbu: "987654321",
-alias: "jane.smith",
-banco: "XYZ Bank",
-},
-{
-id:3,
-nombre: "María Pérez",
-cbu: "124578986",
-alias: "maria.perez",
-banco: "ABC Bank",
-},
-{
-id:3,
-nombre: "José Cordova",
-cbu: "112233445",
-alias: "jose.cordova",
-banco: "XYZ Bank",
-},
+    {
+        id: 1,
+        nombre: "John Doe",
+        cbu: "123456789",
+        alias: "john.doe",
+        banco: "ABC Bank",
+    },
+    {
+        id: 2,
+        nombre: "Jane Smith",
+        cbu: "987654321",
+        alias: "jane.smith",
+        banco: "XYZ Bank",
+    },
+    {
+        id: 3,
+        nombre: "María Pérez",
+        cbu: "124578986",
+        alias: "maria.perez",
+        banco: "ABC Bank",
+    },
+    {
+        id: 3,
+        nombre: "José Cordova",
+        cbu: "112233445",
+        alias: "jose.cordova",
+        banco: "XYZ Bank",
+    },
 ];
 
 //
-const nombres = ["Buscar un Contacto","John Doe", "Jane Smith", "María Pérez", "José Cordova"];
+const nombres = ["Buscar un Contacto", "John Doe", "Jane Smith", "María Pérez", "José Cordova"];
 
 const select = document.getElementById("buscarContacto");
 
@@ -52,36 +52,71 @@ nombres.forEach(nombre => {
     select.appendChild(opcion);
 });
 //
+function formNuevocontacto() {
+    let formNew = "";
+
+    formNew += `
+    <form>
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nombre y apellido:</label>
+                        <input type="email" class="form-control" name="name" id="name"
+                            placeholder="Introduce Nombre y apellido" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="number" class="form-label">número de CBU:</label>
+                        <input type="numer" class="form-control" name="CBU" id="cbu"
+                            placeholder="Introduce número de CBU" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="nickname" class="form-label">Alias:</label>
+                        <input type="email" class="form-control" name="alias" id="alias"
+                            placeholder="Introduce Alias" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="banco" class="form-label">Nombre del Banco:</label>
+                        <input type="email" class="form-control" name="banco" id="banco"
+                            placeholder="Introduce Nombre del Banco" required>
+                    </div>
+                    <div>
+                        <input type="submit" value="Agregar Contacto" class="btn btn-info my-1 col-12 mb-3">
+                    </div>
+                </form>
+    `;
+
+};
+//FORMULARIO
+
+
 
 //FUNCIÓN QUE SE VA A ENCARGAR DE CARGAR LOS CONTACTOS EN LA UL DEL DOM
-function cargarContactos(listaContactos){
+function cargarContactos(listaContactos) {
 
-let acumuladorLi = "";
+    let acumuladorLi = "";
 
-for (const contacto of listaContactos) {
+    for (const contacto of listaContactos) {
 
-//DESECTRUCTURAR LAS PROPIEDADES DEL OBJETO
-let { id, nombre, cbu, alias, banco } = contacto;
+        //DESECTRUCTURAR LAS PROPIEDADES DEL OBJETO
+        let { id, nombre, cbu, alias, banco } = contacto;
 
-//AQUÍ VAMOS ACUMULANDO TANTOS LI COMO CONTACTOS TENGAMOS
-acumuladorLi += `
+        //AQUÍ VAMOS ACUMULANDO TANTOS LI COMO CONTACTOS TENGAMOS
+        acumuladorLi += `
+
 <li class="list-group-item d-flex gap-3">
 <input class="form-check-input " type="radio" name="contacto" value="${cbu}" id="contacto${id}" required>
 <label class="form-check-label" for="contacto${id}">${nombre}, CBU:
 ${cbu}, Alias: ${alias}, Banco: ${banco}</label>
 </li>
-                            
 `;
-};
+    };
 
-//AGREGAMOS LOS ELEMENTOS DE LISTA A LA UL DEL DOM
-document.getElementById("listaContactos").innerHTML = acumuladorLi;
+    //AGREGAMOS LOS ELEMENTOS DE LISTA A LA UL DEL DOM
+    document.getElementById("listaContactos").innerHTML = acumuladorLi;
 };
 
 //FUNCIÓN INICIAL / PRINCIAL
 
-function main(){
-cargarContactos(contactos);
+function main() {
+    cargarContactos(contactos);
 };
 
 main();
@@ -92,38 +127,38 @@ const buscarContactoEl = document.getElementById("buscarContacto");
 
 //AGREGAMOS EL EVENTO INPUT AL ELEMENTO
 
-buscarContactoEl.addEventListener("input", function(event){
-event.preventDefault();
+buscarContactoEl.addEventListener("input", function (event) {
+    event.preventDefault();
 
-let textoBusqueda = buscarContactoEl.value;
+    let textoBusqueda = buscarContactoEl.value;
 
-//DEJAMO EL TEXTO EL INPUT EN MINÚSCULAS
-textoBusqueda = textoBusqueda.toLowerCase();
+    //DEJAMO EL TEXTO EL INPUT EN MINÚSCULAS
+    textoBusqueda = textoBusqueda.toLowerCase();
 
-//FILTRAR LOS CONTACTOS QUE COINCIDAN POR EL TEXTO BUSCADO
+    //FILTRAR LOS CONTACTOS QUE COINCIDAN POR EL TEXTO BUSCADO
 
-let contactosFiltrados = contactos.filter(function(contacto){
-//DESECTRUCTURAR LAS PROPIEDADES DEL OBJETO
-let {nombre, cbu, alias, banco } = contacto;
+    let contactosFiltrados = contactos.filter(function (contacto) {
+        //DESECTRUCTURAR LAS PROPIEDADES DEL OBJETO
+        let { nombre, cbu, alias, banco } = contacto;
 
-nombre = nombre.toLowerCase();
-alias = alias.toLowerCase();
-banco = banco.toLowerCase();
+        nombre = nombre.toLowerCase();
+        alias = alias.toLowerCase();
+        banco = banco.toLowerCase();
 
-let reglaNombre = nombre.includes(textoBusqueda);
-let reglaAlias = alias.includes(textoBusqueda);
-let reglaCbu = cbu.includes(textoBusqueda);
-let reglaBanco = banco.includes(textoBusqueda);
+        let reglaNombre = nombre.includes(textoBusqueda);
+        let reglaAlias = alias.includes(textoBusqueda);
+        let reglaCbu = cbu.includes(textoBusqueda);
+        let reglaBanco = banco.includes(textoBusqueda);
 
-if(reglaNombre || reglaAlias || reglaCbu || reglaBanco){
-return contacto;
-}else{
-return false;
-}
-});
+        if (reglaNombre || reglaAlias || reglaCbu || reglaBanco) {
+            return contacto;
+        } else {
+            return false;
+        }
+    });
 
-//UNA VEZ FILTRADOS LOS CONTACTOS LOS ENVIAMOS A LA FUNCIÓN DE CARGAR CONTACTOS
-cargarContactos(contactosFiltrados);
+    //UNA VEZ FILTRADOS LOS CONTACTOS LOS ENVIAMOS A LA FUNCIÓN DE CARGAR CONTACTOS
+    cargarContactos(contactosFiltrados);
 });
 
 //LÓGICA DE ENVIAR DINERO A UN USUARIO SELECCIONADO
@@ -132,48 +167,48 @@ const formSendMoneyEl = document.getElementById("formSendMoney");
 
 //AGREGAMOS EL EVENTO SUBMIT
 
-formSendMoneyEl.addEventListener("submit", function(event){
+formSendMoneyEl.addEventListener("submit", function (event) {
 
-//DEBEMOS PREVENIR LAS ACCIONES POR DEFECTO
-event.preventDefault();
+    //DEBEMOS PREVENIR LAS ACCIONES POR DEFECTO
+    event.preventDefault();
 
-//CAPTURAMOS LA DATA DEL FORMULARIO
-let dataFormulario = new FormData(formSendMoneyEl);
+    //CAPTURAMOS LA DATA DEL FORMULARIO
+    let dataFormulario = new FormData(formSendMoneyEl);
 
-let monto = dataFormulario.get("monto");
+    let monto = dataFormulario.get("monto");
 
-monto = Number(monto);
+    monto = Number(monto);
 
-//PREGUNTAMOS AL USUARIO SI ESTÁ SEGURO DE TRANSFERIR
-let confirmacion = confirm(`¿Está seguro de transferir la suma de: $ ${monto.toLocaleString("es-CL")}?`);
+    //PREGUNTAMOS AL USUARIO SI ESTÁ SEGURO DE TRANSFERIR
+    let confirmacion = confirm(`¿Está seguro de transferir la suma de: $ ${monto.toLocaleString("es-CL")}?`);
 
-if(!confirmacion){
-//EL RETURN DETIENE LA EJECUCIÓN DEL RESTO DE CÓDIGO
-return;
-}
+    if (!confirmacion) {
+        //EL RETURN DETIENE LA EJECUCIÓN DEL RESTO DE CÓDIGO
+        return;
+    }
 
 
-let cbuContacto = dataFormulario.get("contacto");
+    let cbuContacto = dataFormulario.get("contacto");
 
-//VALIDAR SI EXISTE SALDO DISPONIBLE Y DESCONTAR DE SER POSIBLE
+    //VALIDAR SI EXISTE SALDO DISPONIBLE Y DESCONTAR DE SER POSIBLE
 
-if(balance >= monto){
-//EN ESTE CASO PUEDO TRANSFERIR
-balance = balance - monto;
-let textoMensaje =
-`Se ha transferido correctamente la suma de: $ ${monto.toLocaleString("es-CL")} a la cuenta\nN° ${cbuContacto}.
+    if (balance >= monto) {
+        //EN ESTE CASO PUEDO TRANSFERIR
+        balance = balance - monto;
+        let textoMensaje =
+            `Se ha transferido correctamente la suma de: $ ${monto.toLocaleString("es-CL")} a la cuenta\nN° ${cbuContacto}.
 \nSu nuevo saldo es de: ${balance.toLocaleString("es-CL")}
 `;
-alert(textoMensaje);
+        alert(textoMensaje);
 
-//ACTUALIZAMOS EL BALANCE EN EL SPAN DEL DOM
-balanceEl.textContent = balance.toLocaleString("es-CL");
+        //ACTUALIZAMOS EL BALANCE EN EL SPAN DEL DOM
+        balanceEl.textContent = balance.toLocaleString("es-CL");
 
-//limpiamos el formulario
-formSendMoneyEl.reset();
-cargarContactos(contactos);
+        //limpiamos el formulario
+        formSendMoneyEl.reset();
+        cargarContactos(contactos);
 
-}else{
-alert("Usted no disponible de saldo suficiente");
-}
+    } else {
+        alert("Usted no disponible de saldo suficiente");
+    }
 });
